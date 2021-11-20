09b32c2a68491c5c1c5df27c679104fe8542aca0
raw <subodh.rawat89@gmail.com>
2021-11-20 13:49:00 -0500

    Udpated the module.

--- SpringDemo-java/src/com/demo/repository/PlayerRepository.java
+++ SpringDemo-java/src/com/demo/repository/PlayerRepository.java
@@ -5,5 +5,5 @@ import com.demo.Player;
 import java.util.List;
 
 public interface PlayerRepository {
-    public List<Player> findAll();
+    List<Player> findAll();
 }
--- SpringDemo-java/src/com/demo/service/PlayerService.java
+++ SpringDemo-java/src/com/demo/service/PlayerService.java
@@ -5,5 +5,5 @@ import com.demo.Player;
 import java.util.List;
 
 public interface PlayerService {
-    public List<Player> getPlayerName();
+    List<Player> getPlayerName();
 }
