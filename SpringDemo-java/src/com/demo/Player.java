2fe82521b37fc2e78ca414b7c7d783fc70d6d5ad
raw <subodh.rawat89@gmail.com>
2021-11-20 20:27:57 -0500

    Updating the existing module and adding new xml demo module.

--- SpringDemo-java/src/com/demo/Player.java
+++ SpringDemo-java/src/com/demo/Player.java
@@ -6,15 +6,19 @@ public class Player {
     private String game;
 
     public String getPlayerName() {
+
         return playerName;
     }
     public void setPlayerName(String playerName) {
+
         this.playerName = playerName;
     }
     public String getGame() {
+
         return game;
     }
     public void setGame(String game) {
+
         this.game = game;
     }
 }
--- SpringDemo-java/src/com/demo/service/PlayerServiceImpl.java
+++ SpringDemo-java/src/com/demo/service/PlayerServiceImpl.java
@@ -2,20 +2,21 @@ package com.demo.service;
 
 import com.demo.Player;
 import com.demo.repository.PlayerRepository;
-import com.demo.repository.PlayerRepositoryImpl;
 
 import java.util.List;
 
 public class PlayerServiceImpl implements PlayerService{
 
-    private PlayerRepository playerrepository = new PlayerRepositoryImpl();
+    private PlayerRepository playerrepository ;
 
     @Override
     public List<Player> getPlayerName() {
-       return playerrepository.findAll();
+
+        return playerrepository.findAll();
     }
 
     public void setPlayerRepository(PlayerRepository playerRepository){
+
         this.playerrepository = playerRepository;
     }
 }
